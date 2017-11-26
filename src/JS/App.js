5b314f7d2f4f055dd1f4f9c9984208561e243c36
João Henrique <jppulicidade@gmail.com>
2017-11-26 16:21:45 +0000

    added some CSS and updated pakege.json with react-bootstrap

--- src/JS/App.js
+++ src/JS/App.js
@@ -2,7 +2,25 @@ import React, {Component} from 'react';
 import logo from '../img/logo.svg';
 import FooterInstance from './footer';
 
+class Grid extends React.Component {
+  render() {
+    return (
+      <div>
+        Grid
+      </div>
+    )
+  }
+}
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      generator: 0
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,9 +29,16 @@ class App extends Component {
           <h1 className="App-title">FCC Game of Life by João Henrique</h1>
         </header>
 
+        <div>
+          <h1>The Game Of Life</h1>
+          <Grid/>
+          <h2>Generations: {this.state.generation}</h2>
+        </div>
+
+        {/* FOOTER COMPONENT */}
         <FooterInstance/>
       </div>
-    );
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './CSS/App.css';
 import './CSS/index.css';
 import './CSS/footer.css';
+import './CSS/board.css';
 import App from './JS/App';
 
 ReactDOM.render(
